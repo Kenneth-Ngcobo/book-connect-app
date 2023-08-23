b5eff2152edeea5607ef1a3e80a264c9c5f358a9
Bored_Barney <owenprambanduli@gmail.com>
2023-08-23 10:14:29 +0200

    changes done

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import MainHeader from '@/components/NavigationBar/MainHeader';
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps })
+
+ export default function App({ Component, pageProps })
  {
- 
-  return <Component {...pageProps} />
-  
-}
+  return (
+    <MainHeader/>
+      <Component {...pageProps} />
+   
+  );
+   }
+//pages/_app.js;
+
