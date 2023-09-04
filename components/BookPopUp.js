0cfa8d357bfca5f09093a6f40da1d4ba98974e68
Cbazil <chadbosch08@gmail.com>
2023-09-04 18:06:26 +0200

    Added overlay background, stops from selecting multi cards

--- components/BookPopUp.js
+++ components/BookPopUp.js
@@ -1,7 +1,19 @@
 
+import { useState } from 'react';
 function BookPopUp(props) {
   const { book, closeModal, isOpen } = props
 
+
+  const [overlayed, setOverlayed] = useState('')
+
+  if (isOpen && overlayed !== 'overlay__background') {
+    setOverlayed('overlay__background')
+  } 
+
+  if (!isOpen && overlayed === 'overlay__background') {
+    setOverlayed('')
+  }
+  
   const year = new Date(book.published).getFullYear()
 
   const bookWithPublished = {...book, published: year}
@@ -11,22 +23,24 @@ function BookPopUp(props) {
   }
 
   return (
-    <dialog className="overlay" open={isOpen}>
-      <div className="overlay__preview">
-        <img className="overlay__blur"  src={bookWithPublished.image}/>
-        <img className="overlay__image" src={bookWithPublished.image}/>
-      </div>
-      <div className="overlay__content">
-        <h3 className="overlay__title">{bookWithPublished.title}</h3>
-        <div className="overlay__data">
-          {bookWithPublished.author} ({bookWithPublished.published})
+    <div className={overlayed}>
+      <dialog className="overlay" open={isOpen}>
+        <div className="overlay__preview">
+          <img className="overlay__blur"  src={bookWithPublished.image}/>
+          <img className="overlay__image" src={bookWithPublished.image}/>
         </div>
-          <p className="overlay__data overlay__data_secondary">{bookWithPublished.description}</p>
-      </div>
-      <div className="overlay__row">
-        <button className="overlay__button overlay__button_primary" onClick={handlePopUp}>Close</button>
-      </div>
-    </dialog>
+        <div className="overlay__content">
+          <h3 className="overlay__title">{bookWithPublished.title}</h3>
+          <div className="overlay__data">
+            {bookWithPublished.author} ({bookWithPublished.published})
+          </div>
+            <p className="overlay__data overlay__data_secondary">{bookWithPublished.description}</p>
+        </div>
+        <div className="overlay__row">
+          <button className="overlay__button overlay__button_primary" onClick={handlePopUp}>Close</button>
+        </div>
+      </dialog>
+    </div>
   )
 }
 
